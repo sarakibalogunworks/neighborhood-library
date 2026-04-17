1643c8a5f5ee5b0548589381d92a2d9a761a9d76
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-17 15:37:22 -0400

    first commit

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,4 +1,43 @@
 package com.pluralsight;
 
-public class Class {
+public class Book {
+
+    private int id;
+    private String isbn;
+    private String borrower;
+    private String title;
+    private boolean checkedOut;
+
+
+    public Book(int id, String isbn, String title) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.checkedOut = false;
+        this.borrower = "";
+    }
+
+    public void checkOut(String name) {
+        this.checkedOut = true;
+        this.borrower = name;
+    }
+    public void checkIn() {
+        this.checkedOut = false;
+        this.borrower = "";
+    }
+
+    public int getId() { return id; }
+    public String getIsbn() { return isbn; }
+    public String getTitle() { return title; }
+    public boolean isCheckedOut() { return checkedOut; }
+    public String getBorrower() { return borrower; }
+
+
+
+
+
+
+
+
+
 }
---
+++ src/main/java/com/pluralsight/TheLibraryApp.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class TheLibraryApp {
+
+
+
+}
