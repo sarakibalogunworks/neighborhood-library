0943fa3cbf48608a82427da99629d1c7003faccc
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-20 19:16:36 -0400

    added full lists of books commit

--- src/main/java/com/pluralsight/TheLibraryApp.java
+++ src/main/java/com/pluralsight/TheLibraryApp.java
@@ -15,23 +15,23 @@ public class TheLibraryApp {
         shelf[1] = new Book(2, "978-0-14-044333-2", "Count of Monte Cristo");
         shelf[2] = new Book(3, "978-0-14-044926-6", "The Iliad");
         shelf[3] = new Book(4, "978-0-14-044913-6", "The Odyssey");
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
-        shelf[] = new Book();
+        shelf[4] = new Book(5, "978-0-14-044792-7", "Crime and Punishment");
+        shelf[5] = new Book(6, "978-0-14-044917-4", "The Brothers Karamazov");
+        shelf[6] = new Book((7,"978-0-14-044118-5", "War and Peace");
+        shelf[7] = new Book(8, "978-0-14-044793-4", "Anna Karenina");
+        shelf[8] = new Book(9, "978-0-14-143951-8", "Great Expectations");
+        shelf[9] = new Book(10, "978-0-14-143960-0", "A Tale of Two Cities");
+        shelf[10] = new Book(11, "978-0-14-044281-6", "Don Quixote");
+        shelf[11] = new Book(12, "978-0-14-044082-9", "Madame Bovary");
+        shelf[12] = new Book(13, "978-0-14-044109-3", "The Three Musketeers");
+        shelf[13] = new Book(14, "978-0-14-044027-0", "Les Miserables");
+        shelf[14] = new Book(15, "978-0-14-044095-9", "Frankenstein");
+        shelf[15] = new Book(16, "978-0-14-143947-1", "Dracula");
+        shelf[16] = new Book(17, "978-0-14-044515-2", "The Picture of Dorian Gray");
+        shelf[17] = new Book(18, "978-0-14-044165-9", "The Hunchback of Notre Dame");
+        shelf[18] = new Book(19, "978-0-14-143982-2", "Wuthering Heights");
+        shelf[19] = new Book(20, "978-0-14-143951-8", "Jane Eyre");
+
 
 
 
