4f7b2acac6566eb8e47542d7a67370e3f642b947
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-20 19:07:07 -0400

    added lists of books commit

--- src/main/java/com/pluralsight/TheLibraryApp.java
+++ src/main/java/com/pluralsight/TheLibraryApp.java
@@ -1,6 +1,42 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class TheLibraryApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Book[] shelf = new Book[20];
+        int totalbooks = 0;
+
+        shelf[0] = new Book(1, "978-0-14-017739-8", "Of Human Bondage");
+        shelf[1] = new Book(2, "978-0-14-044333-2", "Count of Monte Cristo");
+        shelf[2] = new Book(3, "978-0-14-044926-6", "The Iliad");
+        shelf[3] = new Book(4, "978-0-14-044913-6", "The Odyssey");
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+        shelf[] = new Book();
+
+
+
+
+    }
 
 
 
